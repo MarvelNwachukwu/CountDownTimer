cfe0a8f30d105c2dc85a63cc79a99b631a0f9fcd
MarvelNwachukwu <marvellousn561@gmail.com>
2019-11-29 03:50:43 +0100

    fixed the timer

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-console.log(time)
+var countDownDate = new Date("Jan 5, 2025 15:37:25").getTime();
+
+var x = setInterval(function() 
+{
+
+  var now = new Date().getTime();
+    
+  
+  var distance = countDownDate - now;
+    
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("demo").innerHTML = seconds + "s";
+}, 1000);
